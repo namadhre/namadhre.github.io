745eb9eaf9a9973bd1fc660ad3b443354f52c684
Mahendra Chinthamgari <mahendrachinthamgari@gmail.com>
2025-05-10 04:50:47 +0530

    Fix asset paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  //base: "/portfolio/", // 👈 Add this line for GitHub Pages
+  base: "/portfolio/", // Base URL for GitHub Pages
   server: {
     host: "::",
     port: 8081,
